0f114c21b89c8bd6abcc86e5f306e04f2a21e0d4
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-05 18:31:02 -0700

    Area of Triangle Program
    Computes the area of a triangle.

---
+++ AbstractionAndRecursion/AreaOfTriangle.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Lab0-Part1
+//
+//  Created by brian on 8/22/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float area, base, height;
+    base = 3.0;
+    height = 4.0;
+
+    // Compute the area of the triangle and output it
+    area = (base * height) / 2.0;
+    cout << "The area of the triangle is: " << area << endl;
+
+    return 0;
+}
