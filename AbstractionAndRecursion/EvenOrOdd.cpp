1e40cf051aa2d7e470b5395d5945498fe19290e8
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-05 18:32:43 -0700

    Even or Odd Program
    Randomly fills an array with values and then fills two sub arrays from that other array. One sub array is filled with all the even values, while the other is filled with all the odd values.

--- AbstractionAndRecursion/EvenOrOdd.cpp
+++ AbstractionAndRecursion/EvenOrOdd.cpp
@@ -1,19 +1,46 @@
 //
 //  main.cpp
-//  InClassQuestion01
+//  Lab0-Part3
 //
-//  Created by brian on 8/30/22.
+//  Created by brian on 8/22/22.
 //
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    int inputInt;
-    cout << "Enter int: ";
-    cin >> inputInt;
+    //defines variables
+    const int SIZE = 50;
+    int oddCount = 0, evenCount = 0;
+    int randomArr[50], oddArr[50], evenArr[50];
+    unsigned seed = time(0);
+    srand(seed);
     
-    inputInt % 2 ? cout << "The number is odd.\n" : cout << "The number is even.\n";
+    //creates array of random ints btwn 1-100
+    for (int i = 0; i < SIZE; i++)
+        randomArr[i] = (rand() % (100)) + 1;
+    
+    //sorts the ints in the randomArr into the evenArry and oddArr
+    for (int i = 0; i < SIZE; i++)
+    {
+        if ((randomArr[i] % 2) == 0)
+            evenArr[evenCount++] = randomArr[i];
+        else
+            oddArr[oddCount++] = randomArr[i];
+    }
+    
+    //outputs the array values
+    cout << "Odd Integers: ";
+    for (int i = 0; i < oddCount; i++)
+        cout << oddArr[i] << " ";
+    cout << endl;
+    
+    cout << "Even Integers: ";
+    for (int i = 0; i < evenCount; i++)
+        cout << evenArr[i] << " ";
+    cout << endl << endl;
     
     return 0;
 }
